860209271d77e63bc1a25c3675bb1dcd118c376b
Raamzic <raamzic@gmail.com>
2015-12-21 02:19:03 -0800

    Messed up slideshow edits.

--- script.js
+++ script.js
@@ -26,3 +26,12 @@ $(function() {
         }
     });
 });
+
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+            $('.fadein :first-child').fadeOut()
+                .next('img').fadeIn()
+                .end().appendTo('.fadein');},
+        3000);
+});
