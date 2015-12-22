5cb9d69dd222fcee73decf56faed18c7f59cd2e5
Raamzic <raamzic@gmail.com>
2015-12-21 20:31:21 -0800

    Adding bootstrap files to local repository instead of through link, slight cosmetic changes with opacity, home anchor added called "name", prep work for adding a toggle nav side bar.

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ $(function() {
             if (target.length) {
                 $('html,body').animate({
                     scrollTop: target.offset().top
-                }, 1500);
+                }, 1000);
                 return false;
             }
         }
@@ -36,6 +36,24 @@ $(function(){
         3000);
 });
 
+/*Scroll Spy*/
+$('body').scrollspy({ target: '#spy', offset:80});
+
+/*Smooth link animation*/
+/*$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+*/
 $("p").click(function(){
     $("p").hide();
 });
